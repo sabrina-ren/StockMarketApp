d98275b1dccd4ef9ea63fa8920a2853cbb21b021
Sabrina Ren <sren22@uwo.ca>
2015-03-10 18:23:42 -0400

    Observe changing bids

--- controllers/marketByOrder.js
+++ controllers/marketByOrder.js
@@ -3,22 +3,15 @@
  */
 StockMarket.MarketByOrderController = Ember.Controller.extend({
     needs:['company'],
+    sortedBids: [],
+    sortingBids: function() {
+        this.set('sortedBids', this.get('model').get('bids'));
+        alert('changed!');
+    }.observes('bids').on('init')
 
-    sortedBids: function(){
-
-        var company = this.get('controllers.company.model');
-
-        return company.get('bids').sort(function(obj1, obj2){
-            return parseFloat(obj2.get('price')) - parseFloat(obj1.get('price'));
-        });
-
-    }.property(this.get('controllers.company.model').get('bids'))
-
-/*
-    sortedBids: function(){
-        return model.get('bids').sort(function(obj1, obj2){
-           return parseFloat(obj2.get('price')) - parseFloat(obj1.get('price'));
-        });
-    }.property(model.get('bids'))
-*/
+    //sortedBids: function(){
+    //    return model.get('bids').sort(function(obj1, obj2){
+    //       return parseFloat(obj2.get('price')) - parseFloat(obj1.get('price'));
+    //    });
+    //}.property(model.get('bids'))
 });
